37b0311e760d1c18d878a584362a044f312b3bd0
grace123456 <19liuzhuoran@bprep.org>
2018-10-16 09:18:18 -0700

    10/16

--- DrawThreeBoxes/src/DrawBox.java
+++ DrawThreeBoxes/src/DrawBox.java
@@ -1,6 +1,8 @@
 public class DrawBox {
     public static void main(String[] args) {
         drawBox(3,4,'A');
+        drawBox(5,6,'B');
+        drawBox(9,10,'C');
     }
           public static void drawBox(int height, int width, char character){
         for (int i = 1; i <= height; i++){
