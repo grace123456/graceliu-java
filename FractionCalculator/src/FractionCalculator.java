5011428b39a8cf24c9a2b9e368028d1f30058625
grace123456 <19liuzhuoran@bprep.org>
2018-12-11 09:20:27 -0800

    12/11

---
+++ FractionCalculator/src/FractionCalculator.java
@@ -0,0 +1,32 @@
+/* Grace Liu
+Ms. Transue
+AP CS
+20 January
+*/
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.lang.String;
+import java.lang.Integer;
+
+public class FractionCalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter number or quit");
+        Scanner input = new Scanner(System.in);
+        String number = input.nextLine();
+        String[] split = number.split("[\\r\\n]+");
+        for (int i = 0; i < split.length; i++){
+            System.out.println(split[i]);
+        }
+
+        while(!(number.equals("quit"))){
+
+
+            System.out.println("Enter next number or quit");
+            number = input.nextLine();
+        }
+
+    }
+
+
+}
