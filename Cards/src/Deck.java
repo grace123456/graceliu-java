0471f96620c25d9e5bc2349730f2f265c3670bf1
grace123456 <19liuzhuoran@bprep.org>
2019-04-12 09:20:25 -0700

    03/21

--- Cards/src/Deck.java
+++ Cards/src/Deck.java
@@ -1,22 +1,62 @@
 import java.util.*;
 public class Deck {
-    public Deck(String[]rank, String[]suit, int[]values){
-        ArrayList<Card> cardList = new ArrayList<Card>();
-        for(int i = 0;i < suit.length;i++){
-            for (int j = 0; j < rank.length; j ++){
-                Card cardToAdd = new Card(values[j],suit[i], rank[j]);
-                System.out.println(cardToAdd);
-                //cardList.add(rank[j] + suit + values);
+    ArrayList<Card> cards;
+    int size;
+
+    public Deck(String[] suit, String[] rank, int[] value) {
+        cards = new ArrayList<Card>();
+        size = cards.size();
+
+        for (int i = 0; i < rank.length; i++) {
+            for (int j = 0; j < suit.length; j++) {
+                cards.add(new Card(value[i], suit[j], rank[i]));
             }
         }
     }
-    /*public boolean isEmpty(){
-        if()
-    } */
-    public static void main(String[] args) {
-        Deck myDeck = new Deck;
-        System.out.println(myDeck);
+
+    // shuffle
+    public void shuffle(){
+        for (int i = 51; i >0; i--) {
+            int r = (int) (Math.random() * 51);
+            Card temp1 = cards.get(r);
+            Card temp2 = cards.get(i);
+            cards.set(r, temp2);
+            cards.set(i, temp1);
+        }
+
+        // print shuffled deck
+        for (int i = 0; i < 51; i++) {
+            System.out.println(cards.get(i));
+        }
     }
+
+
+
+
+
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if(cards.size()==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Card deal() {
+        size = size - 1;
+        if (size > 0) {
+            Card a = cards.get(size);
+            return a;
+        } else {
+            return null;
+        }
+    }
+
 }
 
 
